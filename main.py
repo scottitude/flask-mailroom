c5e2d044ab254b6b5969023e0501b352690000aa
Scott Luse <scottwluse@gmail.com>
2018-06-24 20:32:22 -0700

    Initial commit prior to Heroku

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation
 
 app = Flask(__name__)
 
@@ -15,9 +15,18 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+
+    if request.method == 'POST':
+        donation = Donation(donor=request.form['name'], value=int(request.form['number']))
+        donation.save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6738))
+    port = int(os.environ.get("PORT", 5000)) # 6738
     app.run(host='0.0.0.0', port=port)
 
